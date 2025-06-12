09357fee24ebfc5208dc2a35caec783bc84a57e3
Keerthi <keerthiveluru157@gmail.com>
2025-06-12 07:38:04 +0530

    MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,24 +1,14 @@
 package javaprojects;
 public class MergeSort {
-
-    // Merge two subarrays of arr[].
-    // First subarray is arr[left..mid]
-    // Second subarray is arr[mid+1..right]
     void merge(int arr[], int left, int mid, int right) {
         int len1 = mid - left + 1;
         int len2 = right - mid;
-
         int leftArr[] = new int[len1];
         int rightArr[] = new int[len2];
-
-        // Copy data to temp arrays leftArr[] and rightArr[]
         System.arraycopy(arr, left, leftArr, 0, len1);
         System.arraycopy(arr, mid + 1, rightArr, 0, len2);
-
         int i = 0, j = 0;
         int k = left;
-
-        // Merge the temp arrays back into arr[left..right]
         while (i < len1 && j < len2) {
             if (leftArr[i] <= rightArr[j]) {
                 arr[k] = leftArr[i];
@@ -29,15 +19,12 @@ public class MergeSort {
             }
             k++;
         }
-
-        // Copy the remaining elements of leftArr[], if any
         while (i < len1) {
             arr[k] = leftArr[i];
             i++;
             k++;
         }
 
-        // Copy the remaining elements of rightArr[], if any
         while (j < len2) {
             arr[k] = rightArr[j];
             j++;
@@ -45,17 +32,12 @@ public class MergeSort {
         }
     }
 
-    // Main function that sorts arr[left..right] using merge()
     void sort(int arr[], int left, int right) {
         if (left < right) {
             // Find the middle point to divide the array into two halves
             int mid = left + (right - left) / 2;
-
-            // Sort first and second halves
             sort(arr, left, mid);
             sort(arr, mid + 1, right);
-
-            // Merge the sorted halves
             merge(arr, left, mid, right);
         }
     }
@@ -66,17 +48,12 @@ public class MergeSort {
             System.out.print(arr[i] + " ");
         System.out.println();
     }
-
-    // Driver code
     public static void main(String args[]) {
         int arr[] = {12, 11, 13, 5, 6, 7};
-
         System.out.println("Given array is");
         printArray(arr);
-
         MergeSort ob = new MergeSort();
         ob.sort(arr, 0, arr.length - 1);
-
         System.out.println("\nSorted array is");
         printArray(arr);
     }
